1f95a451d3805279895e5209b5735a0547e68320
parthiv8848 <124236867+parthiv8848@users.noreply.github.com>
2023-11-20 16:25:23 +0530

    first commit

--- src/components/Display.js
+++ src/components/Display.js
@@ -1,53 +1,47 @@
-import React, { useEffect, useState } from "react";
- // Import CSS file for styling
+import React from "react";
+ // Import DisplayPage.css for styling
 
-const Display = () => {
-  const [storedData, setStoredData] = useState([]);
-
-  useEffect(() => {
-    // Retrieve data from local storage
-    const retrievedData = JSON.parse(localStorage.getItem("formData")) || [];
-    setStoredData(retrievedData);
-  }, []);
+const DisplayPage = () => {
+  // Fetch data from local storage or use a sample array for demonstration
+  const storedData = JSON.parse(localStorage.getItem("formData")) || [];
 
   return (
     <div className="display-container">
-      <h2>Stored Form Data</h2>
+      <h2>Stored Data</h2>
       <div className="data-list">
         {storedData.map((data, index) => (
-          <div key={index} className="data-item">
-            {/* Render each piece of stored data */}
-            <p>
+          <div className="data-item" key={index}>
+            <div className="field">
               <span className="field-name">Name:</span> {data.name}
-            </p>
-            <p>
+            </div>
+            <div className="field">
               <span className="field-name">Email:</span> {data.email}
-            </p>
-            <p>
+            </div>
+            <div className="field">
               <span className="field-name">Phone:</span> {data.phone}
-            </p>
-            <p>
+            </div>
+            <div className="field">
               <span className="field-name">Message:</span> {data.message}
-            </p>
-            <p>
+            </div>
+            <div className="field">
               <span className="field-name">Gender:</span> {data.gender}
-            </p>
-            <p>
+            </div>
+            <div className="field">
               <span className="field-name">Birth Date:</span> {data.birthDate}
-            </p>
-            <p>
+            </div>
+            <div className="field">
               <span className="field-name">Country:</span> {data.country}
-            </p>
-            <p>
+            </div>
+            <div className="field">
               <span className="field-name">Occupation:</span> {data.occupation}
-            </p>
-            <p>
+            </div>
+            <div className="field">
               <span className="field-name">Website:</span> {data.website}
-            </p>
-            <p>
+            </div>
+            <div className="field">
               <span className="field-name">Interests:</span> {data.interests}
-            </p>
-            <hr />
+            </div>
+            {/* Add other fields similarly */}
           </div>
         ))}
       </div>
@@ -55,4 +49,4 @@ const Display = () => {
   );
 };
 
-export default Display;
+export default DisplayPage;
