cd7a5dfb9a1239282faccd848112fe3505c2ca1f
parthiv8848 <124236867+parthiv8848@users.noreply.github.com>
2023-11-21 14:51:36 +0530

    firs

--- src/components/Form.js
+++ src/components/Form.js
@@ -54,156 +54,158 @@ const handleSubmit = (e) => {
 
 
   return (
-    <div className="main-container">
-      <Sidebar></Sidebar>
-      <div className="container">
-        <h2>Contact Form</h2>
-        <form className="form" onSubmit={handleSubmit}>
-          <div className="form-row">
-            <label htmlFor="name" className="form-label">
-              Name:
-            </label>
-            <input
-              className="form-input"
-              type="text"
-              id="name"
-              name="name"
-              value={formData.name}
-              onChange={handleChange}
-              required
-            />
-          </div>
-          <div className="form-row">
-            <label className="form-label" htmlFor="email">
-              Email:
-            </label>
-            <input
-              className="form-input"
-              type="email"
-              id="email"
-              name="email"
-              value={formData.email}
-              onChange={handleChange}
-              required
-            />
-          </div>
-          <div className="form-row">
-            <label className="form-label" htmlFor="phone">
-              Phone:
-            </label>
-            <input
-              className="form-input"
-              type="tel"
-              id="phone"
-              name="phone"
-              value={formData.phone}
-              onChange={handleChange}
-              required
-            />
-          </div>
-          <div className="form-row">
-            <label className="form-label" htmlFor="message">
-              Message:
-            </label>
-            <textarea
-              id="message"
-              name="message"
-              value={formData.message}
-              onChange={handleChange}
-            />
-          </div>
-          <div className="form-row">
-            <label className="form-label" htmlFor="gender">
-              Gender:
-            </label>
-            <select
-              id="gender"
-              name="gender"
-              value={formData.gender}
-              onChange={handleChange}
-            >
-              <option value="">Select Gender</option>
-              <option value="male">Male</option>
-              <option value="female">Female</option>
-            </select>
-          </div>
-          <div className="form-row">
-            <label className="form-label" htmlFor="birthDate">
-              Birth Date:
-            </label>
-            <input
-              className="form-input"
-              type="date"
-              id="birthDate"
-              name="birthDate"
-              value={formData.birthDate}
-              onChange={handleChange}
-            />
-          </div>
-          <div className="form-row">
-            <label className="form-label" htmlFor="country">
-              Country:
-            </label>
-            <select
-              id="country"
-              name="country"
-              value={formData.country}
-              onChange={handleChange}
-            >
-              <option value="">Select Country</option>
-              <option value="us">United States</option>
-              <option value="uk">United Kingdom</option>
-              <option value="ca">Canada</option>
-              {/* Add more options */}
-            </select>
-          </div>
-          <div className="form-row">
-            <label className="form-label" htmlFor="occupation">
-              Occupation:
-            </label>
-            <input
-              className="form-input"
-              type="text"
-              id="occupation"
-              name="occupation"
-              value={formData.occupation}
-              onChange={handleChange}
-            />
-          </div>
-          <div className="form-row">
-            <label className="form-label" htmlFor="website">
-              Website:
-            </label>
-            <input
-              className="form-input"
-              type="url"
-              id="website"
-              name="website"
-              value={formData.website}
-              onChange={handleChange}
-            />
-          </div>
-          <div className="form-row">
-            <label htmlFor="interests" className="form-label">
-              Interests:
-            </label>
-            <input
-              className="form-input"
-              type="text"
-              id="interests"
-              name="interests"
-              value={formData.interests}
-              onChange={handleChange}
-            />
-          </div>
-          <div className="form-row">
-            <button type="submit" className="submit-button">
-              Submit
-            </button>
-          </div>
-        </form>
+  
+      <div className="main-container">
+        <Sidebar></Sidebar>
+        <div className="container">
+          <h2>Contact Form</h2>
+          <form className="form" onSubmit={handleSubmit}>
+            <div className="form-row">
+              <label htmlFor="name" className="form-label">
+                Name:
+              </label>
+              <input
+                className="form-input"
+                type="text"
+                id="name"
+                name="name"
+                value={formData.name}
+                onChange={handleChange}
+                required
+              />
+            </div>
+            <div className="form-row">
+              <label className="form-label" htmlFor="email">
+                Email:
+              </label>
+              <input
+                className="form-input"
+                type="email"
+                id="email"
+                name="email"
+                value={formData.email}
+                onChange={handleChange}
+                required
+              />
+            </div>
+            <div className="form-row">
+              <label className="form-label" htmlFor="phone">
+                Phone:
+              </label>
+              <input
+                className="form-input"
+                type="tel"
+                id="phone"
+                name="phone"
+                value={formData.phone}
+                onChange={handleChange}
+                required
+              />
+            </div>
+            <div className="form-row">
+              <label className="form-label" htmlFor="message">
+                Message:
+              </label>
+              <textarea
+                id="message"
+                name="message"
+                value={formData.message}
+                onChange={handleChange}
+              />
+            </div>
+            <div className="form-row">
+              <label className="form-label" htmlFor="gender">
+                Gender:
+              </label>
+              <select
+                id="gender"
+                name="gender"
+                value={formData.gender}
+                onChange={handleChange}
+              >
+                <option value="">Select Gender</option>
+                <option value="male">Male</option>
+                <option value="female">Female</option>
+              </select>
+            </div>
+            <div className="form-row">
+              <label className="form-label" htmlFor="birthDate">
+                Birth Date:
+              </label>
+              <input
+                className="form-input"
+                type="date"
+                id="birthDate"
+                name="birthDate"
+                value={formData.birthDate}
+                onChange={handleChange}
+              />
+            </div>
+            <div className="form-row">
+              <label className="form-label" htmlFor="country">
+                Country:
+              </label>
+              <select
+                id="country"
+                name="country"
+                value={formData.country}
+                onChange={handleChange}
+              >
+                <option value="">Select Country</option>
+                <option value="us">United States</option>
+                <option value="uk">United Kingdom</option>
+                <option value="ca">Canada</option>
+                {/* Add more options */}
+              </select>
+            </div>
+            <div className="form-row">
+              <label className="form-label" htmlFor="occupation">
+                Occupation:
+              </label>
+              <input
+                className="form-input"
+                type="text"
+                id="occupation"
+                name="occupation"
+                value={formData.occupation}
+                onChange={handleChange}
+              />
+            </div>
+            <div className="form-row">
+              <label className="form-label" htmlFor="website">
+                Website:
+              </label>
+              <input
+                className="form-input"
+                type="url"
+                id="website"
+                name="website"
+                value={formData.website}
+                onChange={handleChange}
+              />
+            </div>
+            <div className="form-row">
+              <label htmlFor="interests" className="form-label">
+                Interests:
+              </label>
+              <input
+                className="form-input"
+                type="text"
+                id="interests"
+                name="interests"
+                value={formData.interests}
+                onChange={handleChange}
+              />
+            </div>
+            <div className="form-row">
+              <button type="submit" className="submit-button">
+                Submit
+              </button>
+            </div>
+          </form>
+        </div>
       </div>
-    </div>
+ 
   );
 }
 
